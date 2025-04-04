f93f154e2971a3501f561a65e868fbae1b851548
aleinikovS <slava.aleinikov@gmail.com>
2025-04-05 01:07:38 +0500

    Time: 5 ms (52.73%) | Memory: 45 MB (36.03%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,6 +1,32 @@
 class Solution {
+// BEST DECISION
+
     public int romanToInt(String s) {
-        Map<String, Integer> map = new HashMap();
+        Map<Character, Integer> map = new HashMap();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int result = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (i < s.length() - 1 && map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) {
+                result -= map.get(s.charAt(i));
+            } else {
+                result += map.get(s.charAt(i));
+            }
+        }
+        return result;
+    }
+
+
+// MY FIRST DECISION
+    public int romanToIntMyFirstDecision(String s) {
+                Map<String, Integer> map = new HashMap();
         map.put("I", 1);
         map.put("IV", 4);
         map.put("V", 5);
