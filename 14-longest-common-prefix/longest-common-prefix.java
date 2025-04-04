1321f026eb82fa9e2af80ff994fcc5c6eb9a6795
aleinikovS <slava.aleinikov@gmail.com>
2025-04-05 02:19:06 +0500

    Time: 0 ms (100.00%) | Memory: 41.5 MB (76.58%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -5,30 +5,20 @@ class Solution {
 
         String prefix = strs[0];
         for (int i = 1; i < strs.length; i++) {
-            if (strs[i].length() == 0 || prefix.length() == 0) {
-                return "";
-            }
-
-            if (prefix.length() < strs[i].length()) {
-                prefix = count(strs[i], prefix);
-            } else {
-                prefix = count(prefix, strs[i]);
-            }
+            if (prefix.length() == 0)
+                break;
+            if (strs[i].startsWith(prefix))
+                continue;
+            prefix = substring(strs[i], prefix);
         }
         return prefix;
     }
 
-    private String count(String str, String subStr) {
-        if (str.startsWith(subStr)) {
-            return subStr;
-        } else {
-
-            String newSubStr = "";
-            for (int i = 0; i < subStr.length(); i++) {
-                if (str.charAt(i) != subStr.charAt(i)) return newSubStr;
-                newSubStr += subStr.charAt(i);
-            }
-            return newSubStr;
-        }
+    private static String substring(String str, String prefix) {
+        if (prefix.length() == 0)
+            return "";
+        if (!str.startsWith(prefix))
+            prefix = substring(str, prefix.substring(0, prefix.length() - 1));
+        return prefix;
     }
 }
